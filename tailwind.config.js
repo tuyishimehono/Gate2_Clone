92e60e0f150ed437cfd0d4fbfa21cfdb550645ed
tuyishimehono <tuhonori1@gmail.com>
2024-05-17 10:51:31 +0200

    Added hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ['index.html'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'lato':['Lato','sans-serif'],
+      }
+    },
   },
   plugins: [],
 }
